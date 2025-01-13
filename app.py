531bb11707f62991f84b4d4df37eee82394a2bc0
atnafb <atnafub.ayalew@gmail.com>
2025-01-13 11:10:25 -0500

    jsonify preci analysis
    pushing the jsonify precipitation analysis

--- app.py
+++ app.py
@@ -11,9 +11,24 @@ from sqlalchemy import create_engine, func, inspect
 from flask import Flask, jsonify
 # Creating the flask app
 app = Flask(__name__)
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflect an existing database into a new model
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
 
-# Establishing route 
+# Establishing home route 
 @app.route("/")
 def home():
     return (
@@ -25,7 +40,22 @@ def home():
         f"<center><a href='http://127.0.0.1:5000/api/v1.0/<start>/<end>'>Min, Max, and Avg Temperature API Route</a></center>"
     )
 
+# Precipitation API Rout
+@app.route("/api/v1.0/precipitation")
+def precip(): 
+    # Calculate the date one year from the last date in data set.
+    previous_year_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
+    # Perform a query to retrieve the data and precipitation scores
+    data_preci_score = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= previous_year_date).\
+        all()
+    session.close()
+    # dictionary with the date as the key and the precipitation (pre) as the value
+    Precipitaitons = {date: prcp for date, prcp in data_preci_score} 
+
+    # converting to JSONs
+    return jsonify(Precipitaitons)
 
 
 
