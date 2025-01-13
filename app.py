8888612d0f1e396c36d2e10f02a33a7ad50a10ae
atnafb <atnafub.ayalew@gmail.com>
2025-01-13 10:48:34 -0500

    Pushing HTML Formatting

--- app.py
+++ app.py
@@ -9,6 +9,31 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 
 from flask import Flask, jsonify
+# Creating the flask app
+app = Flask(__name__)
+
+
+# Establishing route 
+@app.route("/")
+def home():
+    return (
+        f"<center><h3><u>This is a Climate Analysis Local API for Hawaii</u></h3></center>"
+        f"<center><h4>Select from one of the available routes:</h4></center>"
+        f"<center><a href='http://127.0.0.1:5000/api/v1.0/precipitation'>Precipitation API Route</a></center><br>"
+        f"<center><a href='http://127.0.0.1:5000/api/v1.0/stations'>List of Stations API Route</a></center><br>"
+        f"<center><a href='http://127.0.0.1:5000/api/v1.0/tobs'>Observed Temperature and Dates API Route</a></center><br>"
+        f"<center><a href='http://127.0.0.1:5000/api/v1.0/<start>/<end>'>Min, Max, and Avg Temperature API Route</a></center>"
+    )
+
+
+
+
+
+
+
+# app louncher 
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 #################################################
